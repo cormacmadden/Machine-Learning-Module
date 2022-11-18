f99af79c5ce51fb9b3c5a8e5b94cd118989e7128
Cormac Madden <maddenc6@ITSERV.SCSS.TCD.IE>
2022-11-18 20:01:05 +0000

    week8 progress

--- Assignment4/week8.py
+++ Assignment4/week8.py
@@ -26,7 +26,6 @@ x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
 print("orig x_train shape:", x_train.shape)
 
-
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
